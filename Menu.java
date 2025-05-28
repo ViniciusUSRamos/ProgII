12a29c1f6074eb4514853b63db5e4cd2cf34905a
Vinícius Ramos <73314551+ViniciusUSRamos@users.noreply.github.com>
2025-05-28 14:05:31 -0300

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -1,43 +1,210 @@
+//VERSAO FUNCIONANDO (28/05/2025)
 import java.util.Arrays;
-import java.util.Collections;
+//import java.util.Collections;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-public class Menu {
+class Menu {
 
-    //private char opcao;
+    public static void main(String[] args) {
+        int opcao;
+        Menu menu = new Menu();
+        Scanner leitor = new Scanner(System.in);
 
 
-    public String[] nomeCrescente(String[] listaNomes){
-        Arrays.sort(listaNomes);
-        return listaNomes;
+        if (args.length == 0) {
+            //System.out.println("Informe o nome do arquivo como argumento.");
+            return;
+        }
+
+        String caminhoArquivo = args[0];
+        String conteudoArquivo = "";
+
+        try {
+            File arquivo = new File(caminhoArquivo);
+            Scanner leitorArquivo = new Scanner(arquivo);
+
+            while (leitorArquivo.hasNextLine()) {
+                conteudoArquivo += leitorArquivo.nextLine() + "#"; // Junta tudo em uma linha só
+            }
+
+            leitorArquivo.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado: " + caminhoArquivo);
+            return;
+        }
+
+        String[] inputTodos = conteudoArquivo.split("#");
+
+
+        int totalPessoas = inputTodos.length / 2; // Arredonda pra baixo se ímpar
+        String[] nomes = new String[totalPessoas];
+        int[] idades = new int[totalPessoas];
+        int iNome = 0;
+        int iIdade = 0;
+
+        for (int i = 0; i < totalPessoas * 2; i++) {
+            if (i % 2 == 0) {
+                nomes[iNome++] = inputTodos[i];
+            } else {
+                idades[iIdade++] = Integer.parseInt(inputTodos[i]);
+            }
+        }
+
+        /*for(int i = 0; i <= inputTodos.length - 1; i++){
+            if (nomes[i] != null) {
+                System.out.println(nomes[i] + "#" + idades[i]);
+            }
+        }*/
+        
+
+        while (true) {
+            System.out.print("1 - Listar ordenado por nome crescente\n" +
+                        "2 - Listar ordenado por nome decrescente\n" +
+                        "3 - Listar ordenado por idade crescente\n" + 
+                        "4 - Listar ordenado por idade decrescente\n" + 
+                        "5 - Localizar um nome\n" + 
+                        "6 - Contar ocorrências de um primeiro nome\n" + 
+                        "7 - Sair\n" + 
+                        "Digite sua opcao: ");
+
+            opcao = leitor.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    menu.nomeCrescente(nomes, idades);
+                    break;
+                case 2:
+                    menu.nomeDecrescente(nomes, idades);
+                    break;
+                case 3:
+                    menu.idadeCrescente(idades, nomes);
+                    break;
+                case 4:
+                    menu.idadeDecrescente(idades, nomes);
+                    break;
+                case 5:
+                    leitor.nextLine();
+                    String nome = leitor.nextLine();
+                    System.out.println(menu.buscaNome(nome, nomes));
+                    break;
+                case 6:
+                    leitor.nextLine(); // limpa quebra de linha
+                    nome = leitor.nextLine();
+                    System.out.println(menu.contaNomes(nome, nomes));
+                    break;
+                case 7:
+                    menu.sair();
+                    break;
+            }
+        }
     }
 
-    public String[] nomeDecrescente(String[] listaNomes){
-        Arrays.sort(listaNomes, Collections.reverseOrder(String.CASE_INSENSITIVE_ORDER));
-        return listaNomes;
+
+    public static void nomeCrescente(String[] nomes, int[] idades) {
+        for (int i = 0; i < nomes.length - 1; i++) {
+            for (int j = 0; j < nomes.length - 1 - i; j++) {
+                if (nomes[j].compareTo(nomes[j + 1]) > 0) {
+                    // troca nomes
+                    String tempNome = nomes[j];
+                    nomes[j] = nomes[j + 1];
+                    nomes[j + 1] = tempNome;
+
+                    // troca idades para manter a posicao certa
+                    int tempIdade = idades[j];
+                    idades[j] = idades[j + 1];
+                    idades[j + 1] = tempIdade;
+                }
+            }
+        }
+        for (int i = 0; i < nomes.length; i++) {
+            if (nomes[i] != null) {
+                System.out.println(nomes[i] + "#" + idades[i]);
+            }
+        }
     }
 
-    public int[] idadeCrescente(int[] idades){
-        Arrays.sort(idades);
-        return idades;
+    public static void nomeDecrescente(String[] nomes, int[] idades) {
+        for (int i = 0; i < nomes.length - 1; i++) {
+            for (int j = 0; j < nomes.length - 1 - i; j++) {
+                if (nomes[j].compareTo(nomes[j + 1]) < 0) {
+                    // troca nomes
+                    String tempNome = nomes[j];
+                    nomes[j] = nomes[j + 1];
+                    nomes[j + 1] = tempNome;
+
+                    // troca idades para manter a posicao certa
+                    int tempIdade = idades[j];
+                    idades[j] = idades[j + 1];
+                    idades[j + 1] = tempIdade;
+                }
+            }
+        }
+        for (int i = 0; i < nomes.length; i++) {
+            if (nomes[i] != null) {
+                System.out.println(nomes[i] + "#" + idades[i]);
+            }
+        }
     }
 
-    public Integer[] idadeDecrescente(int[] idades){
-        //metodo sort com comparador nao funciona para int[], converter para integer antes de seguir
-        Integer[] idadesInteger = Arrays.stream(idades).boxed().toArray(Integer[]::new);
-        Arrays.sort(idadesInteger, Collections.reverseOrder());
-        return idadesInteger;
+    public static void idadeCrescente(int[] idades, String[] nomes) {
+        for (int i = 0; i < idades.length - 1; i++) {
+            for (int j = 0; j < idades.length - 1 - i; j++) {
+                if (idades[j] > idades[j + 1]) {
+                    // troca nomes
+                    String tempNome = nomes[j];
+                    nomes[j] = nomes[j + 1];
+                    nomes[j + 1] = tempNome;
+
+                    // troca idades para manter a posicao certa
+                    int tempIdade = idades[j];
+                    idades[j] = idades[j + 1];
+                    idades[j + 1] = tempIdade;
+                }
+            }
+        }
+        for (int i = 0; i < nomes.length; i++) {
+            if (nomes[i] != null) {
+                System.out.println(nomes[i] + "#" + idades[i]);
+            }
+        }
+    }
+
+    public static void idadeDecrescente(int[] idades, String[] nomes) {
+        for (int i = 0; i < idades.length - 1; i++) {
+            for (int j = 0; j < idades.length - 1 - i; j++) {
+                if (idades[j] < idades[j + 1]) {
+                    // troca nomes
+                    String tempNome = nomes[j];
+                    nomes[j] = nomes[j + 1];
+                    nomes[j + 1] = tempNome;
+
+                    // troca idades para manter a posicao certa
+                    int tempIdade = idades[j];
+                    idades[j] = idades[j + 1];
+                    idades[j + 1] = tempIdade;
+                }
+            }
+        }
+        for (int i = 0; i < nomes.length; i++) {
+            if (nomes[i] != null) {
+                System.out.println(nomes[i] + "#" + idades[i]);
+            }
+        }
     }
 
     public String buscaNome(String nomeBusca, String[] listaNomes){
         //com a lista ordenada, usar busca binaria para encontrar o nome desejado
-        String[] ordenada = nomeCrescente(listaNomes);
+        Arrays.sort(listaNomes);
+        String[] ordenada = listaNomes;
         int contIteracoes = 0;
         int inicio = 0;
         int fim = ordenada.length-1;
 
         //busca binária para encontrar os nomes
         
-        while (true) {
+        while (inicio <= fim) {
             contIteracoes++;
             int meio = (inicio + fim)/2;
             int compara = nomeBusca.compareTo(ordenada[meio]);
@@ -50,10 +217,9 @@ public class Menu {
             }
             else if(compara > 0){
                 inicio = meio + 1;
-            }
-
-            return "INEXISTENTE " + contIteracoes;         
-        }        
+            }         
+        }    
+        return "INEXISTENTE " + contIteracoes;    
     }
 
     public String contaNomes(String nomeBusca, String[] listaNomes){
@@ -75,3 +241,5 @@ public class Menu {
         System.exit(0);
     }
 }
+
+
